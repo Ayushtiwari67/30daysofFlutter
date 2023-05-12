5dc2a6afe627ec8820d6c6c3f7c69f3379997efe
Ayushtiwari67 <ashu04445@gmail.com>
2023-05-12 10:40:32 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:ayush/home_page.dart';
+import 'package:ayush/pages/home_page.dart';
+import 'package:ayush/pages/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,6 +13,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme:
+          ThemeData(brightness: Brightness.dark, primarySwatch: Colors.amber),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+    );
   }
 }
---
+++ lib/pages/login.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text(
+          "Login Page",
+          style: TextStyle(
+              fontSize: 25,
+              color: Colors.redAccent,
+              fontWeight: FontWeight.bold),
+          textScaleFactor: 1.4,
+        ),
+      ),
+    );
+  }
+}
