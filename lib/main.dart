e1a60d81dea8fb1aab89251ced9ee730acc9e09e
Ayushtiwari67 <ashu04445@gmail.com>
2023-05-12 17:57:18 +0530

    Day 4 complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:ayush/pages/home_page.dart';
 import 'package:ayush/pages/login.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,10 +17,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme:
           ThemeData(brightness: Brightness.dark, primarySwatch: Colors.amber),
-      initialRoute: "/home",
+      //initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 
@@ -7,16 +7,48 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 25,
-              color: Colors.redAccent,
-              fontWeight: FontWeight.bold),
-          textScaleFactor: 1.4,
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter Password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Hii codepur");
+                    },
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
